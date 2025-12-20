ee89e76b9ef39603a32c96322e34530ae55e88d3
pxlgenesis <pxlgenesis@gmail.com>
2025-12-20 23:15:06 +0530

    improved structure

--- game.py
+++ game.py
@@ -1,10 +1,14 @@
 import pygame
 
-class Game: #Game Structure template OOPS comppliant
+class Game: #Game Structure template OOPS compliant
+    
     def __init__(self, width: int, height:int): # [<var>: type]
+        
+        pygame.init() #initialize all pygame modules to check
+        
         self.height = height #both stored in class mem by [self.]
         self.width = width
-        pygame.init() #initialize all pygame modules to check
+
         self.screen = pygame.display.set_mode((self.width,self.height)) #Defining game window size
         self.clock = pygame.time.Clock()
         self.game_running = True
@@ -13,16 +17,17 @@ class Game: #Game Structure template OOPS comppliant
         print("here")
 
     def update(self):
-        print("here")
+        self.screen.fill("purple")
+        pygame.display.flip()
+        self.clock.tick(60)
 
     def run(self): #Game Loop
         while self.game_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: #type of event is quit then self game is set to False and terminate the game loop
                     self.game_running = False   
-            self.screen.fill("green")
-            pygame.display.flip()       
-            self.clock.tick(60)
+            self.update()
+            self.handle()
         
 
 def main():
